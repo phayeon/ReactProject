5d608ecf8240b225d4f531b1bd676da12afad43d
phayeon <phayeon1222@gmail.com>
2022-12-16 19:12:03 +0900

    12-16 19:11

--- src/movie/api/index.js
+++ src/movie/api/index.js
@@ -3,4 +3,5 @@ const server = `http://localhost:8000`
 export const strokeCheck = req => axios.get(`${server}/movie/theater_tickets/stroke`, req)
 export const irisGet = req => axios.get(`${server}/movie/theater_tickets/irisget`, req)
 export const irisPost = req => axios.post(`${server}/movie/theater_tickets/irispost`, req)
-export const fashionPost = req => axios.post(`${server}/movie/theater_tickets/fashionpost`, req)
+export const fashionPost = test_num => axios.post(`${server}/movie/theater_tickets/fashion?test_num=${test_num}`, {test_num})
+export const fashionGet = test_num => axios.get(`${server}/movie/theater_tickets/fashion?test_num=${test_num}`)
--- src/movie/components/Fashion.jsx
+++ src/movie/components/Fashion.jsx
@@ -1,24 +1,34 @@
-import { fashionPost } from "../api"
+import { fashionPost, fashionGet } from "../api"
 import { useState } from "react"
 
 const Fashion = () => {
     const [inputs, setInputs] = useState({})
-    const {testNum} = inputs;
-
+    const {test_num} = inputs
     const onChange = e => {
         e.preventDefault()
         const {value, name} = e.target 
         setInputs({...inputs, [name]: value})
     }
 
-    const onClick = e => {
+    const postClick = e => {
         e.preventDefault()
-        const Request = {testNum}
-        alert(`입력하신 정보는 \n ${JSON.stringify(Request)} \n 이 맞습니까?`)
-        fashionPost(Request)
+        alert(`입력하신 정보는 \n ${JSON.stringify(test_num)} \n 이 맞습니까?`)
+        fashionPost(test_num)
         .then((res) => {
-            console.log(`response is ${res.data.result}`)
-            localStorage.setItem('token', JSON.stringify(res.data.result))
+            alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
+        })
+        .catch((err) => {
+            console.log(err)
+            alert('잘못된 접근입니다.')
+        })
+    }
+
+    const getClick = e => {
+        e.preventDefault()
+        alert(`입력하신 정보는 \n ${JSON.stringify(test_num)} \n 이 맞습니까?`)
+        fashionGet(test_num)
+        .then((res)=>{
+            alert(`옷의 카테고리 : ${JSON.stringify(res.data.result)}`)
         })
         .catch((err) => {
             console.log(err)
@@ -29,8 +39,13 @@ const Fashion = () => {
     return (
     <body>
         <h2>패션 정수 입력</h2>
-        <input type="number" name="testNum" placeholder="모델 숫자 입력" onChange={onChange}></input>
-        <button onClick={onClick}>입력</button>
+        <input type="text" name="test_num" placeholder="모델 숫자 입력" onChange={onChange}></input>
+        <form method="post">
+        <button onClick={postClick}>postClick</button><br/>
+        </form>
+        <form method="get">
+        <button onClick={getClick}>getClick</button>
+        </form>
     </body>
     )
 }
